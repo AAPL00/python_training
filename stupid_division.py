1afe3bc2a385d98a4ed5faba3072a61c92fb26f8
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-24 16:16:12 -0400

    Complete stupid?division.py

--- stupid_division.py
+++ stupid_division.py
@@ -1,5 +1,6 @@
-number1 = input()
-number2 = input()
+number1 = int(input("Introduce the first number\n"))  #La función input() captura lo que se introduce como string y debe ser
+number2 = int(input("Introduce the second \n"))       #convertido en este caso con la función int()
 cocient = number1 // number2
-resto = number1 % number2
-print()
+rest = number1 % number2
+print(f"The division between {number1} and {number2} is equal to {cocient} with rest {rest}")
+#Con este formato particular imprimir es muy fácil cuando debes introducir variables en el texto
