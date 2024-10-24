434bb2b8bc374b030bf7c068fb082316fbf3738a
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-24 17:57:24 -0400

    Add matrix.py

---
+++ matrix.py
@@ -0,0 +1,4 @@
+tuple = ((1, 2, 3), (4, 5, 6))          #Las tuplas son tipos de datos estáticos no se pueden alterar los elementos
+tuple2 = ((-1, 0), (0, 1), (1, 1))      #accediendo a sus posiciones ni alterar el tamaño de la tupla
+print(tuple)
+print(tuple2)
