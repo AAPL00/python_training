cd18ff2a37148998c67c7050536cb33b7f2e279e
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-24 16:05:40 -0400

    Add format changes to names

--- hello_name.py
+++ hello_name.py
@@ -1,5 +1,5 @@
 print("Introduce your name")  #Función de escritura por consola acepta todos los tipos de dato built-in de python
-#e incluso objetos que implementen el método __str__() 0 __repr__()
+#e incluso objetos que implementen el método __str__() o __repr__()
 name = input()  #Función de lectura por consola en formato str, acepta un parámetro promt que viene a ser escritura por consola 
 print("Hello", name)
 
