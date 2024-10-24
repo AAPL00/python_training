e2145102b85adef8d8e219ca43545af3d4e926bb
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-24 17:31:45 -0400

    Update the_bad_student

--- the_bad_student.py
+++ the_bad_student.py
@@ -11,10 +11,11 @@ while True:
 for subject in list_of_subjects:
     score = int(input("Introduce your score in " + subject + "\n"))
     if score > 2:
+        print("Estuve aqui")
         list_of_subjects.remove(subject)
 reprobed_subjects = "You reprobed"
 for subject in list_of_subjects:
     reprobed_subjects += (" " + subject)
-    
+print(reprobed_subjects)    
     
     
