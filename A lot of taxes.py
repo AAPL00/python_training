da611018139083a9507ec06292e26ec73c4785b4
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-23 22:04:59 -0400

    Add A lot of taxes.py

---
+++ A lot of taxes.py
@@ -0,0 +1,12 @@
+print("Introduce your income")
+taxes = int(input())
+if taxes < 100000:
+    print("5% taxes")
+elif taxes >= 100000 and taxes < 200000:
+    print("15% taxes")
+elif taxes >= 200000 and taxes < 350000:
+    print("20% taxes")
+elif taxes >= 350000 and taxes < 600000:
+    print("30% taxes")
+elif taxes >= 600000:
+    print("45% taxes")
