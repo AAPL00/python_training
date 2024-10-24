6a2b3f4ba3c040a8e3002966182ae0b8d4a6bb90
AAPL00 <antonioalejandroperezlorenzo9@gmail.com>
2024-10-24 17:24:07 -0400

    Add the_bad_student.py

---
+++ the_bad_student.py
@@ -0,0 +1,20 @@
+list_of_subjects = []
+while True:
+    print("Do you wat to add a subject? Type Yes or No")
+    interrogation = input()
+    if  interrogation == "Yes":
+        list_of_subjects.append(input("Introduce the subject\n"))
+    elif interrogation == "No":
+        break
+    else:
+        print("What do you mean?")
+for subject in list_of_subjects:
+    score = int(input("Introduce your score in " + subject + "\n"))
+    if score > 2:
+        list_of_subjects.remove(subject)
+reprobed_subjects = "You reprobed"
+for subject in list_of_subjects:
+    reprobed_subjects += (" " + subject)
+    
+    
+    
